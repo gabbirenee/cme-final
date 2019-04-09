4bcafd55a08fac23c95575e9eb432bd5ff45d8bf
Gabbi Forsythe <gforsythe17@gmail.com>
2019-04-08 21:41:51 -0400

    added the form and images that were provided

---
+++ SSform.php
@@ -0,0 +1,48 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>contact</title>
+</head>
+
+<body>
+
+<?php
+
+$firstName = $_POST["fName"];
+$lastName = $_POST["lName"];
+$email = $_POST["Email"];
+$comment = $_POST["comments"];
+$subject = "Sweet Serendipity Contact Form";
+
+//Enter your email address
+$to = "";
+
+
+//Prepare Email Body Text
+	$Body = "";
+	$Body .= "Name: ";
+	$Body .= $firstName . ' ' . $lastName;
+	$Body .= "\n";
+	$Body .= "Email: ";
+	$Body .= $email;
+	$Body .= "\n";
+	$Body .= "Comments: ";
+	$Body .= $comment;
+	$Body .= "\n";
+	
+//Send Email
+$send_contact= mail($to,$subject,$Body);
+if($send_contact) {
+	print "<meta http-equiv=\"refresh\" content=\"0;URL=Thanks.php\">";
+}
+ else {
+	 print "<meta http-equiv=\"refresh\" content=\"0;URL=contact.html\">";
+}
+?>
+
+
+            
+</body>
+</html>
+
